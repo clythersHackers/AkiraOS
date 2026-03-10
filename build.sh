1b28419439bf08d4208f376c9008d8ed1ea47fd9
ArturR0k3r <artur2003s.ass@gmail.com>
2026-03-11 00:40:54 +0200

    fix(build): fix ANSI color codes not rendering in help for build.sh

--- build.sh
+++ build.sh
@@ -49,13 +49,13 @@ PORT=""
 BAUD="921600"
 
 # Colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-BOLD='\033[1m'
-NC='\033[0m'
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+BLUE=$'\033[0;34m'
+CYAN=$'\033[0;36m'
+BOLD=$'\033[1m'
+NC=$'\033[0m'
 
 # =============================================================================
 # Board Discovery (auto-loaded from boards/*.conf metadata)
